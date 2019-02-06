fb416153116eaa57f4bb7d5375b53e33f8e231b9
auyer <rafael.passos@redes.unb.br>
2019-02-06 17:16:07 -0200

    restructured config module

--- config/config.go
+++ config/config.go
@@ -9,67 +9,76 @@ import (
 	"os"
 )
 
-var (
-	LogFile      io.Writer
-	ConfigParams *configStruct
-)
-
-type configStruct struct {
+// ConfigurationStruct models the necessary configs for an API
+type ConfigurationStruct struct {
 	LogLocation     string `json:"LogLocation"`
-	HttpPort        string `json:"HttpPort"`
-	HttpsPort       string `json:"HttpsPort"`
+	HTTPPort        string `json:"HTTPPort"`
+	HTTPSPort       string `json:"HTTPSPort"`
 	TLSKeyLocation  string `json:"TLSKeyLocation"`
 	TLSCertLocation string `json:"TLSCertLocation"`
-	DbUser          string `json:"DatabaseUser"`
-	DbPassword      string `json:"DatabasePassword"`
-	DbName          string `json:"DatabaseName"`
-	DbHost          string `json:"DatabaseHost"`
-	DbPort          string `json:"DatabasePort"`
+	DbAddress       string `json:"DbAddress"`
 	Debug           string `json:"Debug"`
+	LogFile         io.Writer
 }
 
-func ReadConfig() error {
+// ReadFromEnv reads the configuration from enviroment variables
+func ReadFromEnv() ConfigurationStruct {
+	var configParams ConfigurationStruct
+	configParams.LogLocation = os.Getenv("LOGLOCATION")
+	configParams.LogFile = setLogFile(configParams.LogLocation)
+	configParams.HTTPPort = os.Getenv("HTTPPORT")
+	configParams.HTTPSPort = os.Getenv("HTTPSPORT")
+	configParams.TLSKeyLocation = os.Getenv("TLSKEYLOCATION")
+	configParams.TLSCertLocation = os.Getenv("TLSCERTLOCATION")
+	configParams.DbAddress = os.Getenv("DBADDRESS")
+	return configParams
 
-	file, err := ioutil.ReadFile("./config.json")
+}
+
+// ReadFromFile reads from a config file or enviroment variables
+func ReadFromFile(fileloc string) (ConfigurationStruct, error) {
+	var configParams ConfigurationStruct
+	file, err := ioutil.ReadFile(fileloc)
 
 	if err != nil {
 		log.Print(err.Error())
-		return err
+		return configParams, err
 	}
 
-	err = json.Unmarshal(file, &ConfigParams)
+	err = json.Unmarshal(file, &configParams)
 
 	if err != nil {
 		log.Print(err.Error())
-		return err
+		return configParams, err
 	}
-	if ConfigParams.Debug == "true" {
+	if configParams.Debug == "true" {
 		fmt.Println("Configuration Parameters")
 		fmt.Println(string(file))
 	}
 
-	if ConfigParams.LogLocation != "" {
-		if _, err := os.Stat(ConfigParams.LogLocation); os.IsNotExist(err) {
-			fileLog, fileErr := os.Create(ConfigParams.LogLocation)
-			if fileErr != nil {
-				fmt.Println(fileErr)
-				fileLog = os.Stdout
-			} else {
-				fmt.Println("Writing logs to file " + ConfigParams.LogLocation)
+	configParams.LogFile = setLogFile(configParams.LogLocation)
+
+	return configParams, nil
+}
+
+func setLogFile(logLocation string) io.Writer {
+	if logLocation != "" {
+		if _, err := os.Stat(logLocation); os.IsNotExist(err) {
+			fileLog, fileErr := os.Create(logLocation)
+			if fileErr == nil {
+				fmt.Println("Writing logs to file " + logLocation)
+				return fileLog
 			}
-			LogFile = fileLog
+			fmt.Println(fileErr)
 		} else {
-			fileLog, fileErr := os.OpenFile(ConfigParams.LogLocation, os.O_RDWR|os.O_APPEND, 0660)
-			if fileErr != nil {
-				fmt.Println(fileErr)
-				fileLog = os.Stdout
-			} else {
-				fmt.Println("Writing logs to file " + ConfigParams.LogLocation)
+			fileLog, fileErr := os.OpenFile(logLocation, os.O_RDWR|os.O_APPEND, 0660)
+			if fileErr == nil {
+				fmt.Println("Writing logs to file " + logLocation)
+				return fileLog
 			}
-			LogFile = fileLog
+			fmt.Println(fileErr)
 		}
-	} else {
-		LogFile = os.Stdout
 	}
-	return nil
+	return os.Stdout
+
 }
